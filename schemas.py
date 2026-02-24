3587714abb4b641f603e1739f7b5011840dd003c
FranklinMonro <fbg2468@gmail.com>
2026-02-24 05:09:05 +0000

    part_6

--- schemas.py
+++ schemas.py
@@ -1,11 +1,11 @@
-import datetime
+from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
-    email: EmailStr = Field(min_length=5, max_length=120)
+    email: EmailStr = Field(max_length=120)
 
 
 class UserCreate(UserBase):
@@ -14,9 +14,16 @@ class UserCreate(UserBase):
 
 class UserResponse(UserBase):
     model_config = ConfigDict(from_attributes=True)
+
     id: int
     image_file: str | None
     image_path: str
+
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 
 class PostBase(BaseModel):
@@ -25,12 +32,18 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    user_id: int
+    user_id: int  # TEMPORARY
+
+
+class PostUpdate(BaseModel):
+    title: str | None = Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1)
 
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
+
     id: int
     user_id: int
-    date_posted: datetime.datetime
+    date_posted: datetime
     author: UserResponse
