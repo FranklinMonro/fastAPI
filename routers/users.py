c07332f43fd258b469f46333eb1379131712995f
FranklinMonro <fbg2468@gmail.com>
2026-02-24 07:11:56 +0000

    part_9

--- routers/users.py
+++ routers/users.py
@@ -67,7 +67,10 @@ async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_d
             detail="User not found",
         )
     result = await db.execute(
-        select(models.Post).options(selectinload(models.Post.author)).where(models.Post.user_id == user_id),
+        select(models.Post)
+        .options(selectinload(models.Post.author))
+        .where(models.Post.user_id == user_id)
+        .order_by(models.Post.date_posted.desc()),
     )
     posts = result.scalars().all()
     return posts
